a76b4ab8fb7ce685addd0b68bd47177b56ad0f8b
Sander Peguero <sanderpeguero.21@gmail.com>
2022-09-28 19:45:56 -0400

    ViewModel Implementation

--- app/src/main/java/edu/ucne/parcial1_sander/ui/articulos/articulosViewModel.kt
+++ app/src/main/java/edu/ucne/parcial1_sander/ui/articulos/articulosViewModel.kt
@@ -1,4 +1,36 @@
 package edu.ucne.parcial1_sander.ui.articulos
 
-//Model
-//Model Repository
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.parcial1_sander.model.Articulo
+import edu.ucne.parcial1_sander.repository.articuloRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class articulosViewModel @Inject constructor(
+    val repository: articuloRepository
+): ViewModel() {
+
+    var descripcion by mutableStateOf("")
+    var marca by mutableStateOf("")
+    var existencia by mutableStateOf(0.0)
+
+    fun save(){
+        viewModelScope.launch {
+            repository.InsertarArticulo(
+                Articulo(
+                    descripcion = descripcion,
+                    marca = marca,
+                    existencia = existencia
+                )
+            )
+        }
+    }
+
+}
+

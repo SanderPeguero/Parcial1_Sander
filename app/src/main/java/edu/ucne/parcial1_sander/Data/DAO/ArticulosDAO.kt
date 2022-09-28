803fbd01bae7ee281f061c7e9ec7dd60327029d8
Sander Peguero <sanderpeguero.21@gmail.com>
2022-09-28 19:17:54 -0400

    Creation of ArticulosDAO

--- app/src/main/java/edu/ucne/parcial1_sander/Data/DAO/ArticulosDAO.kt
+++ app/src/main/java/edu/ucne/parcial1_sander/Data/DAO/ArticulosDAO.kt
@@ -1,9 +1,26 @@
 package edu.ucne.parcial1_sander.Data.DAO
 
 import androidx.room.*
+import edu.ucne.parcial1_sander.model.Articulo
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ArticulosDAO{
+
+    @Insert
+    suspend fun InsertarArticulo(articulo: Articulo)
+
+    @Update
+    suspend fun ActualizarArticulo(articulo: Articulo)
+
+    @Delete
+    suspend fun EliminarArticulo(articulo: Articulo)
+
+    @Query("select * from Articulos where articuloId = :articuloId ")
+    fun getArticulo (articuloId: Int): Flow<Articulo>
+
+    @Query("select * from Articulos")
+    fun getAll(): Flow<List<Articulo>>
 
 
 }

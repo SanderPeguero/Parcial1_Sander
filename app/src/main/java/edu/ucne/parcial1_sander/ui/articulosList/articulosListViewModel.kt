cff4f57daea762e8b5eda7e6de706c231a746684
Sander Peguero <sanderpeguero.21@gmail.com>
2022-09-28 19:08:47 -0400

    Added Methods of the Repo in ViewModel

--- app/src/main/java/edu/ucne/parcial1_sander/ui/articulosList/articulosListViewModel.kt
+++ app/src/main/java/edu/ucne/parcial1_sander/ui/articulosList/articulosListViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.ucne.parcial1_sander.model.Articulo
+import edu.ucne.parcial1_sander.repository.articuloRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,18 +19,18 @@ data class articuloList(
 
 @HiltViewModel
 class articulosListViewModel @Inject constructor(
-    //val repository: -repository name-
+    val repository: articuloRepository
 ): ViewModel(){
     private val _uiState = MutableStateFlow(articuloList())
     val uiState: StateFlow<articuloList> = _uiState.asStateFlow()
 
     init{
         viewModelScope.launch {
-//            repository.getAll().collect { list ->
-//                _uiState.update{
-//                    it.copy( articulo = list)
-//                }
-//            }
+            repository.getAll().collect { list ->
+                _uiState.update{
+                    it.copy( articulo = list)
+                }
+            }
         }
     }
 
@@ -37,7 +38,7 @@ class articulosListViewModel @Inject constructor(
         viewModelScope.launch(
             Dispatchers.IO
         ) {
-//            repository.BorrarArticulo(articulo)
+            repository.EliminarArticulo(articulo)
         }
     }
 }

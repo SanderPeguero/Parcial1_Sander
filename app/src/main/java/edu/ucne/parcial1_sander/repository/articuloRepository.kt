0406bb55b8cc4ee26d3076d3b6cff275354978f1
Sander Peguero <sanderpeguero.21@gmail.com>
2022-09-28 18:58:03 -0400

    Creation of articulosRepository

---
+++ app/src/main/java/edu/ucne/parcial1_sander/repository/articuloRepository.kt
@@ -0,0 +1,23 @@
+package edu.ucne.parcial1_sander.repository
+
+import edu.ucne.parcial1_sander.model.Articulo
+import edu.ucne.parcial1_sander.Data.AppDatabase
+import javax.inject.Inject
+
+class articuloRepository @Inject constructor(
+    val db: AppDatabase
+){
+    suspend fun InsertarArticulo(articulo: Articulo){
+        db.articulosDAO.Insertar(articulo)
+    }
+
+    suspend fun ActualizarArticulo(articulo: Articulo){
+        db.articulosDAO.Actualizar(articulo)
+    }
+
+    suspend fun EliminarArticulo(articulo: Articulo){
+        db.articulosDAO.Eliminar(articulo)
+    }
+
+    fun GetList() = db.articulosDao.getall()
+}

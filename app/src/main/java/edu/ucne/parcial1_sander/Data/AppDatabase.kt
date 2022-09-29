e8778c6b3d09dc897cc6e94dac6bb761614f5b09
Sander Peguero <sanderpeguero.21@gmail.com>
2022-09-28 20:01:42 -0400

    Changing a Letter of a val

--- app/src/main/java/edu/ucne/parcial1_sander/Data/AppDatabase.kt
+++ app/src/main/java/edu/ucne/parcial1_sander/Data/AppDatabase.kt
@@ -2,7 +2,7 @@ package edu.ucne.parcial1_sander.Data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import edu.ucne.parcial1_sander.Data.DAO.articulosDAO
+import edu.ucne.parcial1_sander.Data.DAO.ArticulosDAO
 import edu.ucne.parcial1_sander.model.Articulo
 
 @Database(
@@ -13,6 +13,6 @@ import edu.ucne.parcial1_sander.model.Articulo
 
 abstract class AppDatabase: RoomDatabase() {
 
-    abstract val articulosDAO: articulosDAO
+    abstract val articulosDAO: ArticulosDAO
 
 }
